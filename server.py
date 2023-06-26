ae39a88774468fcc383bb4a3e70a20c7c8bcb69e
BooyahSquadians69 <jiveshkalra69@gmail.com>
2023-06-27 03:04:24 +0530

    comments

--- server.py
+++ server.py
@@ -1,44 +1,58 @@
-# Importing
-from flask import Flask, render_template, request
+# Importing Packages
+from flask import Flask, render_template, request 
 import requests
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 load_dotenv()
 
 # AI API Setup
 hugging_face_token = os.getenv("HUGGING_FACE_TOKEN")
 API_URL = "https://api-inference.huggingface.co/models/OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5"
 headers = {"Authorization": f"Bearer {hugging_face_token}"}
-
-app = Flask(__name__)
-
 conversations = []
 
-@app.route('/', methods=['POST','GET'])
-@app.route('/home', methods=['POST','GET'])
+# Initializing app
+app = Flask(__name__)
+
+# Home route handler
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/home', methods=['POST', 'GET'])
 def home():
     if request.method == "POST":
-        global conversations
+        # Get user input from form
         msg_input = request.form['msg_input']
+
+        # Clear conversations if input is 'cls'
         if msg_input == "cls":
             conversations = []
             msg_input = "Hey"
+
+        # Get chatbot response
         response = get_chatbot_response(prompt=msg_input)
+
+        # Store conversation in conversations list
         conversation = [msg_input, response]
         conversations.append(conversation)
-        return render_template('index.html', msg_input=msg_input,conversations=conversations)
-        # return json.dumps({'msg_input':msg_input})
+
+        # Render template with updated conversation
+        return render_template('index.html', msg_input=msg_input, conversations=conversations)
     else:
+        # Render template initially
         return render_template("index.html")
 
+
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
+    # Send POST request to API
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
 
 def get_chatbot_response(prompt):
-    output = query({"inputs": f"<|prompter|>{prompt}<|endoftext|><|assistant|>",})
-    return output[0]['generated_text'].split('<|assistant|>')[-1] 
+    # Get chatbot response from API
+    output = query({"inputs": f"{prompt}",})
+    return output[0]['generated_text'].split('')[-1]
 
 
-if __name__ == "__main__": 
-    app.run(debug=True) 
+if __name__ == "__main__":
+    # Run the Flask app in debug mode
+    app.run(debug=True)
